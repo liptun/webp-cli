91e09b831ba76f289cd7259dcdac99df449dedfa
Rafał Karczmarzyk <rkarczmarzyk@biostat.com.pl>
2020-07-24 08:38:16 +0200

    Add color output and format code

--- webp.js
+++ webp.js
@@ -1,14 +1,20 @@
+const fs = require('fs')
+
 const webp = require('webp-converter')
+const chalk = require('chalk')
 
 if ( !process.argv[3] ) {
-    console.log('Filename is required')
+    console.log(chalk.red('Filename is required'))
     return
 }
 
-const fileName = process.argv[2] + '\\' + process.argv[3]
+const fileDir = process.argv[2].split('\\').join('/')
+const fileName = `${fileDir}/${process.argv[3]}`
 const fileNameWebP = fileName.split('.').slice(0, -1).join('.') + '.webp'
 
-webp.cwebp(fileName, fileNameWebP, '-q 80')
-.then((response) => {
-    console.log(response)
-})
+if ( fs.existsSync(fileName) ) {
+    webp.cwebp(fileName, fileNameWebP, '-q 80')
+    .then((response) => {
+        console.log(chalk.green(response))
+    })
+}
